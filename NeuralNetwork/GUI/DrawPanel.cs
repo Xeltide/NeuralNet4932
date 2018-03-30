ba086448f5ce5ad8561dad9de8bb8b7f9fa8fb64
Joshua Abe <joshua.t.abe@gmail.com>
2018-03-29 17:13:12 -0700

    Added centering for numbers and anti-aliasing for scaling, improving accuracy

--- NeuralNetwork/GUI/DrawPanel.cs
+++ NeuralNetwork/GUI/DrawPanel.cs
@@ -12,10 +12,13 @@ namespace NeuralNetwork.GUI
     {
         private bool isDrawing = false;
         public Bitmap image;
+        public Bitmap scaledImage;
         private Point lastPos;
+        private float xMin, xMax, yMin, yMax;
 
         public DrawPanel()
         {
+            ResetMinMaxDraw();
             MouseDown += new MouseEventHandler(DrawStart);
             MouseMove += new MouseEventHandler(DrawMove);
             MouseUp += new MouseEventHandler(DrawEnd);
@@ -29,9 +32,38 @@ namespace NeuralNetwork.GUI
             Invalidate();
         }
 
+        private void ResetMinMaxDraw()
+        {
+            xMin = float.MaxValue;
+            yMin = float.MaxValue;
+            xMax = -1;
+            yMax = -1;
+        }
+
+        private void SetMinMaxDraw(float x, float y)
+        {
+            if (x > xMax)
+            {
+                xMax = x;
+            }
+            if (x < xMin)
+            {
+                xMin = x;
+            }
+            if (y > yMax)
+            {
+                yMax = y;
+            }
+            if (y < yMin)
+            {
+                yMin = y;
+            }
+        }
+
         private void DrawStart(object sender, MouseEventArgs e)
         {
             lastPos = e.Location;
+            SetMinMaxDraw(e.X, e.Y);
             isDrawing = true;
         }
 
@@ -39,6 +71,7 @@ namespace NeuralNetwork.GUI
         {
             if (isDrawing)
             {
+                SetMinMaxDraw(e.X, e.Y);
                 Graphics g = Graphics.FromImage(image);
                 g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
                 g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
@@ -57,6 +90,7 @@ namespace NeuralNetwork.GUI
 
         private void DrawEnd(object sender, MouseEventArgs e)
         {
+            SetMinMaxDraw(e.X, e.Y);
             isDrawing = false;
         }
 
@@ -73,14 +107,30 @@ namespace NeuralNetwork.GUI
 
         public double[,] GetImageData()
         {
-            Bitmap scaled = new Bitmap(image, 28, 28);
+            scaledImage = new Bitmap(28, 28);
+
+            Graphics g = Graphics.FromImage(scaledImage);
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+            float ratio = 28.0f / ClientSize.Width;
+            float scaledW = (xMax - xMin) * ratio;
+            float scaledH = (yMax - yMin) * ratio;
+            float w = (28 - scaledW) / 2.0f;
+            float h = (28 - scaledH) / 2.0f;
+            float scaledX = xMin * ratio;
+            float scaledY = yMin * ratio;
+            float adjustX = scaledX - w;
+            float adjustY = scaledY - h;
+            g.DrawImage(image, new Rectangle((int)-adjustX, (int)-adjustY, 28, 28));
+            
             double[,] output = new double[784, 1];
-            for (int row = 0; row < scaled.Height; row++)
+            for (int row = 0; row < scaledImage.Height; row++)
             {
-                for (int col = 0; col < scaled.Width; col++)
+                for (int col = 0; col < scaledImage.Width; col++)
                 {
-                    double r = scaled.GetPixel(col, row).A;
-                    output[col + (row * scaled.Width), 0] = scaled.GetPixel(col, row).A / 255.0;
+                    output[col + (row * scaledImage.Width), 0] = scaledImage.GetPixel(col, row).A / 255.0;
                 }
             }
 
@@ -90,6 +140,7 @@ namespace NeuralNetwork.GUI
         public void ClearPanel()
         {
             image = new Bitmap(ClientSize.Width, ClientSize.Height);
+            ResetMinMaxDraw();
             Invalidate();
         }
     }
